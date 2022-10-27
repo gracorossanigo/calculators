5b8ccc8d7366416509bdc10ded5a2075265ec087
gracorossanigo <rossanigograco@gmail.com>
2022-10-27 14:18:15 -0300

    add query selectors for each button in calculator

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const add = function (a, b){
-    return a + b;
+    return +a + +b;
 }
 const substract = function (a, b){
-    return a - b;
+    return +a - +b;
 }
 const multiply = function (a, b){
-    return a * b;
+    return +a * +b;
 }
 const divide = function (a, b){
-    return a / b;
+    return +a / +b;
 }
 const operate = function (operator, a, b){
     return operator(a, b);
 }
+const container = document.querySelector('#main-container');
+const visor = container.querySelector('#visor');
+const keyboard = container.querySelector('#keyboard');
+const out = visor.querySelector('h1');
+const ac = keyboard.querySelector('#ac');
+const parenthesis = keyboard.querySelector('#()');
+const percentage = keyboard.querySelector('#%');
+const division = keyboard.querySelector('#/');
+const seven = keyboard.querySelector('#7');
+const eight = keyboard.querySelector('#8');
+const nine = keyboard.querySelector('#9');
+const x = keyboard.querySelector('#x');
+const four = keyboard.querySelector('#4');
+const five = keyboard.querySelector('#5');
+const six = keyboard.querySelector('#6');
+const minus = keyboard.querySelector('#-');
+const one = keyboard.querySelector('#one');
+const two = keyboard.querySelector('#two');
+const three = keyboard.querySelector('#three');
+const plus = keyboard.querySelector('#+');
+const zero = keyboard.querySelector('#0');
+const dot = keyboard.querySelector('#.');
+const back = keyboard.querySelector('#<=');
+const equals = keyboard.querySelector('#=');
